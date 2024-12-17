4434cb3a1c60a4e1852171b76b3b73fc43f65338
Mark Peters <mapeters101@earthlink.net>
2024-12-17 15:18:18 -0500

    Work on lesson041 to 9:20.

--- lesson041.cpp
+++ lesson041.cpp
@@ -29,6 +29,29 @@ int main() {
 	cout << endl << endl;
 
 
+	short short_var{ -32768 };
+	short int short_int{ 455 };
+	signed short signed_short{ 122 };
+	signed short int signed_short_int { -456 };
+	unsigned short int unsigned_short_int{ 456 };
+
+	int int_var{ 55 };
+	signed signed_var{ 66 };
+	signed int signed_int{ 77 };
+	unsigned int unsigned_int{ 77 };
+
+	long long_var{ 88 };
+	long int long_int{ 33 };
+	signed long signed_long{ 44 };
+	signed long int signed_long_int{ 44 };
+	unsigned long int unsigned_long_int{ 44 };
+
+	long long long_long{ 888 };
+	long long int long_long_int { 999 };
+	signed long long signed_long_long{ 444 };
+	signed long long int signed_long_long_int{ 1234 };
+	unsigned long long int unsigned_long_long_int{ 1234 };
+
 
 
 
