6607cbc518b85f7cf1ee5d109106be4d3ffe4556
Mark Peters <mapeters101@earthlink.net>
2024-12-18 18:39:14 -0500

    Work on lesson042 to 17:20.

--- lesson042.cpp
+++ lesson042.cpp
@@ -18,7 +18,23 @@ int main() {
 	cout << "number1 is: " << number1 << endl;
 	cout << "number2 is: " << number2 << endl;
 	cout << "number3 is: " << number3 << endl;
+	cout << "----------------\n\n" << endl;
 
+
+	float number4a{ 1924000023.0f };
+	cout << "number4a: " << number4a << endl;
+	double number4b{ 1924000023.0f };
+	cout << "number4b: " << number4b << endl;
+	double number4c{ 1924000023.0 };
+	cout << "number4c: " << number4c << endl;
+	cout << endl;
+
+	float number4a2( 1924000023.0f );
+	cout << "number4a2: " << number4a2 << endl;
+	double number4b2( 1924000023.0f );
+	cout << "number4b2: " << number4b2 << endl;
+	double number4c2( 1924000023.0 );
+	cout << "number4c2: " << number4c2 << endl;
 
 
 	return 0;
