d7f1ef1a083ed61e3a1a84795dd801fbb3a46c6b
Mark Peters <mapeters101@earthlink.net>
2024-12-18 21:11:41 -0500

    Work on lesson043.

---
+++ lesson043.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+
+/*
+int main() {
+	bool red_light{ true };
+	bool green_light{ false };
+
+	if (red_light == true) {
+		cout << "Stop!" << endl;
+	}
+	else {
+		cout << "Go through!" << endl;
+	}
+
+	if (green_light) {
+		cout << "The light is green!" << endl;
+	}
+	else {
+		cout << "The light is NOT green!" << endl;
+	}
+
+	cout << "sizeof(bool) : " << sizeof(bool) << endl;
+
+	cout << endl;
+	cout << "red_light : " << red_light << endl;
+	cout << "green_light : " << green_light << endl;
+
+	cout << std::boolalpha;
+	cout << "red_light : " << red_light << endl;
+	cout << "green_light : " << green_light << endl;
+
+	return 0;
+}
+*/
