4938f838b3d44fcbbb0d0c36265b3831a0379cd6
Mark Peters <mapeters101@earthlink.net>
2024-12-19 12:50:44 -0500

    Work on lesson046.

---
+++ lesson046.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+int main() {
+	int var1{ 123 };
+	cout << "var1 : " << var1 << endl;
+	var1 = 55;
+	cout << "var1 : " << var1 << endl;
+
+	cout << endl;
+
+	double var2{ 44.55 };
+	cout << "var2 : " << var2 << endl;
+	var2 = 99.99;
+	cout << "var2 : " << var2 << endl;
+
+	cout << endl;
+
+	bool state{ false };
+	cout << boolalpha;
+	cout << "state : " << state << endl;
+	state = true;
+	cout << "state : " << state << endl;
+
+	cout << endl;
+
+	auto var3{ 333u };
+	cout << "var3 : " << var3 << endl;
+	var3 = -22;
+	cout << "var3 : " << var3 << endl;
+
+	return 0;
+}
+*/
+
